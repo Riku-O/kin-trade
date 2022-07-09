6f39242e9fee798639a5cf8e3d4514de7f4e1b88
Riku-O <baskelife-rikur11@ezweb.ne.jp>
2022-07-09 19:28:54 +0900

    pumaの出力をdocker logsで見るために追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.default_log_file = '/proc/1/fd/1'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
