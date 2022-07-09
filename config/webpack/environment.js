7a3a6c5e9c668f1890e1f502d56fecc7387f85bb
Riku-O <baskelife-rikur11@ezweb.ne.jp>
2022-07-09 19:43:24 +0900

    デフォルトの postcss-loader は PostCSS 8 を対応していないので強制的に上書き

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,23 +1,16 @@
 const { environment } = require("@rails/webpacker");
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
+const { VueLoaderPlugin } = require("vue-loader");
+const vue = require("./loaders/vue");
 
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
+environment.plugins.prepend("VueLoaderPlugin", new VueLoaderPlugin());
+environment.loaders.prepend("vue", vue);
 module.exports = environment;
 
 function hotfixPostcssLoaderConfig(subloader) {
   const subloaderName = subloader.loader;
   if (subloaderName === "postcss-loader") {
-    if (subloader.options.postcssOptions) {
-      console.log(
-        "\x1b[31m%s\x1b[0m",
-        "Remove postcssOptions workaround in config/webpack/environment.js"
-      );
-    } else {
-      subloader.options.postcssOptions = subloader.options.config;
-      delete subloader.options.config;
-    }
+    subloader.options.postcssOptions = subloader.options.config;
+    delete subloader.options.config;
   }
 }
 
