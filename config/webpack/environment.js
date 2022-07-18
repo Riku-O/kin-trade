129600e3bfd0bc9209867efab59a4efe897ea443
Riku-O <baskelife-rikur11@ezweb.ne.jp>
2022-07-18 17:39:01 +0900

    jqueryの導入

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,14 @@
 const { environment } = require("@rails/webpacker");
 
+const webpack = require("webpack");
+environment.plugins.prepend(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery/src/jquery",
+    jQuery: "jquery/src/jquery",
+  })
+);
+
 function hotfixPostcssLoaderConfig(subloader) {
   const subloaderName = subloader.loader;
   if (subloaderName === "postcss-loader") {
