763951867090e0a7daee018d1f61724ea7aaa97b
Riku-O <baskelife-rikur11@ezweb.ne.jp>
2022-10-08 23:16:38 +0900

    slick.jsを修正してカルーセルできるようにした。あとprevとnextの矢印の表示

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ require("@rails/activestorage").start();
 require("channels");
 require("jquery");
 import "stylesheets/application.css";
-require("jquery/src/slider.js");
+require("../slick.js");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
